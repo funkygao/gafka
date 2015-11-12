3d85b10f0e4b98d14280d8316d8706a17fde5e82
funkygao <funky.gao@gmail.com>
2015-11-12 16:19:29 +0800

    topology feature done

--- cmd/gafka/command/topology.go
+++ cmd/gafka/command/topology.go
@@ -1,9 +1,14 @@
 package command
 
 import (
+	"fmt"
+	"sort"
 	"strings"
 
+	"github.com/funkygao/gafka/config"
+	"github.com/funkygao/gafka/zk"
 	"github.com/funkygao/gocli"
+	"github.com/funkygao/golib/color"
 )
 
 type Topology struct {
@@ -11,7 +16,50 @@ type Topology struct {
 }
 
 func (this *Topology) Run(args []string) (exitCode int) {
+	if len(args) > 0 {
+		for _, zone := range args {
+			ensureZoneValid(zone)
+
+			zkzone := zk.NewZkZone(zk.DefaultConfig(zone, config.ZonePath(zone)))
+			this.displayZoneTopology(zone, zkzone)
+		}
+
+		return
+	}
+
+	// print all brokers on all zones by default
+	forAllZones(func(zone string, zkzone *zk.ZkZone) {
+		this.displayZoneTopology(zone, zkzone)
+	})
+
 	return
+
+}
+
+func (this *Topology) displayZoneTopology(zone string, zkzone *zk.ZkZone) {
+	this.Ui.Output(zone)
+
+	instances := make(map[string][]int)
+	zkzone.WithinBrokers(func(cluster string, brokers map[string]*zk.Broker) {
+		for _, broker := range brokers {
+			if _, present := instances[broker.Host]; !present {
+				instances[broker.Host] = make([]int, 0)
+			}
+			instances[broker.Host] = append(instances[broker.Host], broker.Port)
+		}
+	})
+
+	// sort by host ip
+	sortedHosts := make([]string, 0, len(instances))
+	for host, _ := range instances {
+		sortedHosts = append(sortedHosts, host)
+	}
+	sort.Strings(sortedHosts)
+
+	for _, host := range sortedHosts {
+		this.Ui.Output(fmt.Sprintf("\t%s %+v", color.Green(host),
+			instances[host]))
+	}
 
 }
 
