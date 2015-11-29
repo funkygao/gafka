11846b27addc06e5b75f4401dbe1b94137b24021
funkygao <funky.gao@gmail.com>
2015-11-29 22:41:01 +0800

    pub gateway client shows the pub reponse result

--- cmd/kateway/client/pub.go
+++ cmd/kateway/client/pub.go
@@ -12,13 +12,16 @@ import (
 )
 
 var (
-	addr string
-	n    int
+	addr   string
+	msgKey string
+	n      int
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", "http://localhost:9191", "pub kateway addr")
-	flag.IntVar(&n, "n", 10, "run pub how many times")
+	flag.IntVar(&n, "n", 50, "run pub how many times")
+	flag.StringVar(&msgKey, "key", "", "pub message key")
+
 	flag.Parse()
 
 	http.DefaultClient.Timeout = time.Second * 30
@@ -42,7 +45,7 @@ func main() {
 		b.Reset()
 		b.WriteString(fmt.Sprintf("hello@%s  %d", time.Now(), i))
 		req, _ := http.NewRequest("POST",
-			fmt.Sprintf("%s/v1/topics/foobar?key=xxx", addr),
+			fmt.Sprintf("%s/v1/topics/foobar?key=%s", addr, msgKey),
 			strings.NewReader(b.String()))
 		req.Header.Set("Pubkey", "mypubkey")
 		response, err := client.Do(req)
@@ -50,7 +53,11 @@ func main() {
 			panic(err)
 		}
 
-		ioutil.ReadAll(response.Body)
+		b, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(b))
 		response.Body.Close() // reuse the connection
 	}
 
