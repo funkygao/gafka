acf96c5fd31c77bc86e43bd0c121e7a51fa5a00f
funkygao <funky.gao@gmail.com>
2015-12-01 10:42:28 +0800

    a dumb pub store implementation, for performance testing

---
+++ cmd/kateway/store/dumb/pub.go
@@ -0,0 +1,30 @@
+package dumb
+
+import (
+	"sync"
+
+	"github.com/funkygao/gafka/cmd/kateway/meta"
+)
+
+type pubStore struct {
+}
+
+func NewPubStore(meta meta.MetaStore, hostname string, wg *sync.WaitGroup,
+	shutdownCh <-chan struct{}, debug bool) *pubStore {
+	return &pubStore{}
+}
+
+func (this *pubStore) Start() (err error) {
+	return
+}
+
+func (this *pubStore) SyncPub(cluster string, topic, key string,
+	msg []byte) (partition int32, offset int64, err error) {
+	return
+}
+
+func (this *pubStore) AsyncPub(cluster string, topic, key string,
+	msg []byte) (err error) {
+
+	return
+}
