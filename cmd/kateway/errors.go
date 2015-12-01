2f30cdd9e2064136998511a39eef5c8a814a4858
funkygao <funky.gao@gmail.com>
2015-12-01 11:38:32 +0800

    dumb consumer done

--- cmd/kateway/errors.go
+++ cmd/kateway/errors.go
@@ -5,6 +5,7 @@ import (
 )
 
 var (
+	// FIXME dup with store/kafka
 	ErrTooManyConsumers = errors.New("consumers within a group cannot exceed partition count")
 	ErrRebalancing      = errors.New("rebalancing, please come back after a while")
 )
