88bff4b3f324f554550064ddc302060981e16ec1
funkygao <funky.gao@gmail.com>
2015-12-16 18:12:02 +0800

    fix bugs of 'gk topology'

--- cmd/gk/command/topology.go
+++ cmd/gk/command/topology.go
@@ -17,8 +17,9 @@ import (
 )
 
 type Topology struct {
-	Ui          cli.Ui
-	Cmd         string
+	Ui  cli.Ui
+	Cmd string
+
 	zone        string
 	hostPattern string
 	verbose     bool
@@ -46,6 +47,7 @@ func (this *Topology) Run(args []string) (exitCode int) {
 				return
 			}
 			time.Sleep(time.Second * 5)
+			refreshScreen()
 		}
 
 		return
@@ -61,65 +63,90 @@ func (this *Topology) Run(args []string) (exitCode int) {
 			return
 		}
 		time.Sleep(time.Second * 5)
+		refreshScreen()
 	}
 
 	return
 }
 
 type brokerHostInfo struct {
-	ports             []int
-	leadingPartitions int                // being leader of how many partitions
-	topics            map[string][]int32 // detailed leading topics info {topic: partitionIds}
-	msgsInStock       int64
-	topicMsgs         map[string]int64
+	uptimes         map[int]time.Time  // key is broker tcp port
+	tcpPorts        []int              // listening tcp ports
+	topicPartitions map[string][]int32 // detailed leading topics info {topic: partitionIds}
+	topicMsgs       map[string]int64
 }
 
 func newBrokerHostInfo() *brokerHostInfo {
 	return &brokerHostInfo{
-		ports:     make([]int, 0),
-		topics:    make(map[string][]int32),
-		topicMsgs: make(map[string]int64),
+		tcpPorts:        make([]int, 0),
+		uptimes:         make(map[int]time.Time),
+		topicPartitions: make(map[string][]int32),
+		topicMsgs:       make(map[string]int64),
 	}
 }
 
-func (this *brokerHostInfo) addPort(port int) {
-	this.ports = append(this.ports, port)
+func (this *brokerHostInfo) leadingPartitions() int {
+	r := 0
+	for _, p := range this.topicPartitions {
+		r += len(p)
+	}
+	return r
+}
+
+func (this *brokerHostInfo) totalMsgsInStock() int64 {
+	r := int64(0)
+	for _, n := range this.topicMsgs {
+		r += n
+	}
+	return r
+}
+
+func (this *brokerHostInfo) addPort(port int, uptime time.Time) {
+	this.tcpPorts = append(this.tcpPorts, port)
+	this.uptimes[port] = uptime
 }
 
 func (this *brokerHostInfo) addTopicPartition(topic string, partitionId int32) {
-	if _, present := this.topics[topic]; !present {
-		this.topics[topic] = []int32{partitionId}
+	if _, present := this.topicPartitions[topic]; !present {
+		this.topicPartitions[topic] = []int32{partitionId}
 	} else {
-		this.topics[topic] = append(this.topics[topic], partitionId)
+		this.topicPartitions[topic] = append(this.topicPartitions[topic], partitionId)
 	}
 }
 
 func (this *Topology) displayZoneTopology(zkzone *zk.ZkZone) {
 	this.Ui.Output(zkzone.Name())
 
-	instances := make(map[string]*brokerHostInfo)
+	// {cluster: {topic: brokerHostInfo}}
+	brokerInstances := make(map[string]map[string]*brokerHostInfo)
+
 	zkzone.ForSortedBrokers(func(cluster string, liveBrokers map[string]*zk.BrokerZnode) {
 		if len(liveBrokers) == 0 {
+			this.Ui.Warn(fmt.Sprintf("empty brokers in cluster[%s]", cluster))
 			return
 		}
+
+		brokerInstances[cluster] = make(map[string]*brokerHostInfo)
 
 		for _, broker := range liveBrokers {
 			if !patternMatched(broker.Host, this.hostPattern) {
 				continue
 			}
 
-			if _, present := instances[broker.Host]; !present {
-				instances[broker.Host] = newBrokerHostInfo()
+			if _, present := brokerInstances[cluster][broker.Host]; !present {
+				brokerInstances[cluster][broker.Host] = newBrokerHostInfo()
 			}
-			instances[broker.Host].addPort(broker.Port)
+			brokerInstances[cluster][broker.Host].addPort(broker.Port, broker.Uptime())
 		}
 
 		// find how many partitions a broker is leading
 		zkcluster := zkzone.NewCluster(cluster)
 		brokerList := zkcluster.BrokerList()
 		if len(brokerList) == 0 {
+			this.Ui.Warn(fmt.Sprintf("empty brokers in cluster[%s]", cluster))
 			return
 		}
+
 		kfk, err := sarama.NewClient(brokerList, sarama.NewConfig())
 		if err != nil {
 			this.Ui.Error(color.Red("    %+v %s", brokerList, err.Error()))
@@ -145,37 +172,82 @@ func (this *Topology) displayZoneTopology(zkzone *zk.ZkZone) {
 				oldestOffset, err := kfk.GetOffset(topic, partitionID, sarama.OffsetOldest)
 				swallow(err)
 
-				instances[host].msgsInStock += (latestOffset - oldestOffset)
-				instances[host].topicMsgs[topic] += (latestOffset - oldestOffset)
-				instances[host].leadingPartitions++
-				instances[host].addTopicPartition(topic, partitionID)
+				brokerInstances[cluster][host].topicMsgs[topic] += (latestOffset - oldestOffset)
+				brokerInstances[cluster][host].addTopicPartition(topic, partitionID)
 			}
 		}
 	})
 
-	// sort by host ip
-	sortedHosts := make([]string, 0, len(instances))
-	for host, _ := range instances {
+	hosts := make(map[string]struct{})
+	zkzone.ForSortedClusters(func(zkcluster *zk.ZkCluster) {
+		for host, _ := range brokerInstances[zkcluster.Name()] {
+			hosts[host] = struct{}{}
+		}
+	})
+	sortedHosts := make([]string, 0)
+	for host, _ := range hosts {
 		sortedHosts = append(sortedHosts, host)
 	}
 	sort.Strings(sortedHosts)
 
+	// sort by host ip
+	sortedClusters := make([]string, 0, len(brokerInstances))
+	for c, _ := range brokerInstances {
+		sortedClusters = append(sortedClusters, c)
+	}
+	sort.Strings(sortedClusters)
+
 	for _, host := range sortedHosts {
+		tn := 0
+		pn := 0
+		mn := int64(0)
+		ports := make([]int, 0)
+		for _, cluster := range sortedClusters {
+			if _, present := brokerInstances[cluster][host]; !present {
+				continue
+			}
+
+			tn += len(brokerInstances[cluster][host].topicPartitions)
+			pn += brokerInstances[cluster][host].leadingPartitions()
+			mn += brokerInstances[cluster][host].totalMsgsInStock()
+			ports = append(ports, brokerInstances[cluster][host].tcpPorts...)
+		}
+
 		this.Ui.Output(fmt.Sprintf("  %s leading: %2dT %3dP %15sM ports %2d:%+v",
 			color.Green("%15s", host),
-			len(instances[host].topics),
-			instances[host].leadingPartitions,
-			gofmt.Comma(instances[host].msgsInStock),
-			len(instances[host].ports),
-			instances[host].ports))
+			tn,
+			pn,
+			gofmt.Comma(mn),
+			len(ports),
+			ports))
 
 		if this.verbose {
-			for topic, partitions := range instances[host].topics {
-				this.Ui.Output(fmt.Sprintf("%40s: %15sM P%2d %+v",
-					topic,
-					gofmt.Comma(instances[host].topicMsgs[topic]),
-					len(partitions), partitions))
+			for _, cluster := range sortedClusters {
+				if _, present := brokerInstances[cluster][host]; !present {
+					continue
+				}
+
+				for _, tcpPort := range brokerInstances[cluster][host].tcpPorts {
+					this.Ui.Output(fmt.Sprintf("%40d %s", tcpPort,
+						gofmt.PrettySince(brokerInstances[cluster][host].uptimes[tcpPort])))
+				}
 			}
+
+			for _, cluster := range sortedClusters {
+				if _, present := brokerInstances[cluster][host]; !present {
+					continue
+				}
+
+				this.Ui.Output(color.Magenta("%30s", cluster))
+				for topic, partitions := range brokerInstances[cluster][host].topicPartitions {
+					this.Ui.Output(fmt.Sprintf("%40s: %15sM P%2d %+v",
+						topic,
+						gofmt.Comma(brokerInstances[cluster][host].topicMsgs[topic]),
+						len(partitions), partitions))
+				}
+
+			}
+
 		}
 	}
 }
