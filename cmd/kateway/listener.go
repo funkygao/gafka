9cecbb918ff0d6837883c214863dd6ad279b1e4f
funkygao <funky.gao@gmail.com>
2015-12-23 10:48:39 +0800

    trace each socket conn/close

--- cmd/kateway/listener.go
+++ cmd/kateway/listener.go
@@ -3,7 +3,48 @@ package main
 import (
 	"net"
 	"sync"
+
+	log "github.com/funkygao/log4go"
 )
+
+type fastListener struct {
+	net.Listener
+	gw *Gateway
+}
+
+func FastListener(l net.Listener, gw *Gateway) net.Listener {
+	return &fastListener{l, gw}
+}
+
+func (l *fastListener) Accept() (net.Conn, error) {
+	c, err := l.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Trace("%s got conn %s", l.Listener.Addr(), c.RemoteAddr())
+	if l.gw != nil && !options.disableMetrics {
+		l.gw.pubMetrics.ConnAccept.Inc(1)
+		l.gw.pubMetrics.PubConcurrent.Inc(1)
+	}
+
+	return &fastListenerConn{c, l.gw}, nil
+}
+
+type fastListenerConn struct {
+	net.Conn
+	gw *Gateway
+}
+
+func (c *fastListenerConn) Close() error {
+	log.Trace("%s closing conn %s", c.Conn.LocalAddr(), c.Conn.RemoteAddr())
+
+	err := c.Conn.Close()
+	if c.gw != nil && !options.disableMetrics {
+		c.gw.pubMetrics.PubConcurrent.Dec(1)
+	}
+	return err
+}
 
 type limitListener struct {
 	net.Listener
@@ -28,10 +69,12 @@ func (l *limitListener) Accept() (net.Conn, error) {
 		return nil, err
 	}
 
+	log.Trace("%s got conn %s", l.Listener.Addr(), c.RemoteAddr())
 	if l.gw != nil && !options.disableMetrics {
 		l.gw.pubMetrics.ConnAccept.Inc(1)
 		l.gw.pubMetrics.PubConcurrent.Inc(1)
 	}
+
 	return &limitListenerConn{Conn: c, release: l.release, gw: l.gw}, nil
 }
 
@@ -43,6 +86,8 @@ type limitListenerConn struct {
 }
 
 func (c *limitListenerConn) Close() error {
+	log.Trace("%s closing conn %s", c.Conn.LocalAddr(), c.Conn.RemoteAddr())
+
 	err := c.Conn.Close()
 	if c.gw != nil && !options.disableMetrics {
 		c.gw.pubMetrics.PubConcurrent.Dec(1)
