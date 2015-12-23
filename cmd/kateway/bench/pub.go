7f8c74060a5b0ca7f2b8ead3001283c4d7544d66
funkygao <funky.gao@gmail.com>
2015-12-23 12:27:25 +0800

    tiresome testing

---
+++ cmd/kateway/bench/pub.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"sync/atomic"
+
+	"github.com/funkygao/gafka/cmd/kateway/api"
+)
+
+var (
+	c       int
+	addr    string
+	n       int64
+	msgSize int
+)
+
+func main() {
+	flag.IntVar(&c, "c", 10, "client concurrency")
+	flag.IntVar(&msgSize, "sz", 100, "msg size")
+	flag.StringVar(&addr, "h", "http://localhost:9191", "pub http addr")
+	flag.Parse()
+
+	for i := 0; i < c; i++ {
+		go pubGatewayLoop(i)
+	}
+
+	select {}
+}
+
+func pubGatewayLoop(seq int) {
+	cf := api.DefaultConfig()
+	client := api.NewClient("app1", cf)
+	client.Connect(addr)
+
+	var (
+		err error
+		msg string
+		no  int64
+	)
+
+	for {
+		no = atomic.AddInt64(&n, 1)
+		msg = fmt.Sprintf("%2d %10d %s", seq, no, strings.Repeat("X", msgSize))
+		err = client.Publish("foobar", "v1", "", []byte(msg))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
