90cdd003812735e5d33f9324ecd859e5555c4d5c
funkygao <funky.gao@gmail.com>
2016-01-07 21:05:49 +0800

    log when sarama panics

---
+++ cmd/kateway/store/kafka/init.go
@@ -0,0 +1,12 @@
+package kafka
+
+import (
+	"github.com/Shopify/sarama"
+	log "github.com/funkygao/log4go"
+)
+
+func init() {
+	sarama.PanicHandler = func(err interface{}) {
+		log.Warn("sarama got panic: %v", err)
+	}
+}
