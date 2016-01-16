68bc8fed1ff33753974034e85810bb75158b9643
funkygao <funky.gao@gmail.com>
2016-01-16 14:05:25 +0800

    ehaproxy integration with rsyslog guide

--- cmd/ehaproxy/command/deploy.go
+++ cmd/ehaproxy/command/deploy.go
@@ -14,13 +14,15 @@ type Deploy struct {
 	Ui  cli.Ui
 	Cmd string
 
-	root string
+	root    string
+	rsyslog bool
 }
 
 func (this *Deploy) Run(args []string) (exitCode int) {
 	cmdFlags := flag.NewFlagSet("deploy", flag.ContinueOnError)
 	cmdFlags.Usage = func() { this.Ui.Output(this.Help()) }
 	cmdFlags.StringVar(&this.root, "p", defaultPrefix, "")
+	cmdFlags.BoolVar(&this.rsyslog, "rsyslog", true, "")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
 	}
@@ -33,6 +35,10 @@ func (this *Deploy) Run(args []string) (exitCode int) {
 	swalllow(err)
 	err = os.MkdirAll(fmt.Sprintf("%s/src", this.root), 0755)
 	swalllow(err)
+
+	for _, errcode := range []int{400, 401, 500, 502, 503, 504} {
+		this.touch(fmt.Sprintf("%s/logs/%d.http", this.root, errcode))
+	}
 
 	srcPath := fmt.Sprintf("%s/src/haproxy-1.6.3.tar.gz", this.root)
 	b, _ := Asset("templates/haproxy-1.6.3.tar.gz")
@@ -49,7 +55,33 @@ func (this *Deploy) Run(args []string) (exitCode int) {
 	this.Ui.Info(fmt.Sprintf("cp %s to /etc/init.d/ehaproxy", initPath))
 	this.Ui.Info(fmt.Sprintf("chkconfig --add ehaproxy"))
 
+	if this.rsyslog {
+		this.Ui.Info("install and setup rsyslog for haproxy")
+
+		this.configRsyslog()
+	}
+
 	return
+}
+
+func (this *Deploy) configRsyslog() {
+	this.Ui.Output(fmt.Sprintf(`
+vim  /etc/rsyslog.d/haproxy.conf
+$ModLoad imudp
+$UDPServerRun 514
+local3.*     /var/log/haproxy.log
+
+vim /etc/sysconfig/rsyslog
+SYSLOGD_OPTIONS=”-c 2 -r -m 0″
+#-c 2 使用兼容模式，默认是 -c 5
+#-r 开启远程日志
+#-m 0 标记时间戳。单位是分钟，为0时，表示禁用该功能	
+		`))
+}
+
+func (this *Deploy) touch(fn string) {
+	err := ioutil.WriteFile(fn, []byte{}, 0644)
+	swalllow(err)
 }
 
 func (this *Deploy) Synopsis() string {
@@ -66,6 +98,9 @@ Options:
 
     -p prefix dir
       Defaults %s
+
+    -rsyslog
+      Display rsyslog integration with haproxy
 
 `, this.Cmd, this.Cmd, defaultPrefix)
 	return strings.TrimSpace(help)
