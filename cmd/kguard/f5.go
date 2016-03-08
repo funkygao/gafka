a03370bb7b59794d689398566ba5adba2ba8c9dc
funkygao <funky.gao@gmail.com>
2016-03-08 13:22:37 +0800

    more metrics lines to compare

--- cmd/kguard/f5.go
+++ cmd/kguard/f5.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/funkygao/gafka/mpool"
 	"github.com/funkygao/go-metrics"
 	log "github.com/funkygao/log4go"
 )
@@ -20,8 +21,11 @@ type MonitorF5 struct {
 	tick time.Duration
 	wg   *sync.WaitGroup
 
-	latencyWithF5    metrics.Histogram
-	latencyWithoutF5 metrics.Histogram
+	latencyWithF5WithGateway       metrics.Histogram
+	latencyWithoutF5WithoutGateway metrics.Histogram
+	latencyWithoutF5WithGateway    metrics.Histogram
+
+	httpConn *http.Client
 }
 
 func (this *MonitorF5) Run() {
@@ -30,28 +34,69 @@ func (this *MonitorF5) Run() {
 	ticker := time.NewTicker(this.tick)
 	defer ticker.Stop()
 
-	this.latencyWithF5 = metrics.NewRegisteredHistogram("api.with.f5", nil, metrics.NewExpDecaySample(1028, 0.015))
-	this.latencyWithoutF5 = metrics.NewRegisteredHistogram("api.without.f5", nil, metrics.NewExpDecaySample(1028, 0.015))
+	this.httpConn = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 1,
+			DisableKeepAlives:   true, // enable http conn reuse
+		},
+	}
+
+	this.latencyWithF5WithGateway = metrics.NewRegisteredHistogram("latency.api.f5yes.gwyes", nil, metrics.NewExpDecaySample(1028, 0.015))
+	this.latencyWithoutF5WithoutGateway = metrics.NewRegisteredHistogram("latency.api.f5no.gwno", nil, metrics.NewExpDecaySample(1028, 0.015))
+	this.latencyWithoutF5WithGateway = metrics.NewRegisteredHistogram("latency.api.f5no.gwyes", nil, metrics.NewExpDecaySample(1028, 0.015))
 	for {
 		select {
 		case <-this.stop:
 			return
 
 		case <-ticker.C:
-			this.callWithF5()
-			this.callWithoutF5()
+			this.callWithF5WithGateway()
+			this.callWithoutF5WithoutGateway()
+			this.callWithoutF5WithGateway()
 		}
 	}
 }
 
-func (this *MonitorF5) callWithF5() {
+func (this *MonitorF5) callWithF5WithGateway() {
 	url := "http://api.ffan.com/pubsub/v1/pub/alive"
-	this.callHttp(url, this.latencyWithF5)
+	this.callHttp(url, this.latencyWithF5WithGateway)
 }
 
-func (this *MonitorF5) callWithoutF5() {
+func (this *MonitorF5) callWithoutF5WithoutGateway() {
 	url := "http://sub.sit.ffan.com:9191/alive"
-	this.callHttp(url, this.latencyWithoutF5)
+	this.callHttp(url, this.latencyWithoutF5WithoutGateway)
+}
+
+func (this *MonitorF5) callWithoutF5WithGateway() {
+	url := "10.209.36.67/pubsub/v1/pub/alive"
+	host := "api.ffan.com"
+
+	buf := mpool.BytesBufferGet()
+	defer mpool.BytesBufferPut(buf)
+
+	var t time.Time
+	for i := 0; i < HttpLoop; i++ {
+		t = time.Now()
+
+		buf.Reset()
+		req, err := http.NewRequest("GET", url, buf)
+		if err != nil {
+			log.Error("%s %v", url, err)
+			continue
+		}
+
+		req.Header.Set("Host", host)
+
+		resp, err := this.httpConn.Do(req)
+		if err != nil {
+			log.Error("%s %v", url, err)
+			continue
+		}
+
+		resp.Body.Close()
+		this.latencyWithoutF5WithGateway.Update(time.Since(t).Nanoseconds() / MsInNano)
+	}
+
 }
 
 func (this *MonitorF5) callHttp(url string, h metrics.Histogram) {
@@ -69,6 +114,8 @@ func (this *MonitorF5) callHttp(url string, h metrics.Histogram) {
 			log.Error("%s %s", url, resp.Status)
 			continue
 		}
+
+		resp.Body.Close()
 
 		h.Update(time.Since(t).Nanoseconds() / MsInNano)
 	}
