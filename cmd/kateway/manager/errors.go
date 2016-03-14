d2b535e473e5a3c18a7254606916a131f4a2d8e7
funkygao <funky.gao@gmail.com>
2016-03-14 15:59:43 +0800

    a constructive err msg

--- cmd/kateway/manager/errors.go
+++ cmd/kateway/manager/errors.go
@@ -7,6 +7,6 @@ import (
 var (
 	ErrEmptyParam         = errors.New("auth with empty identity")
 	ErrPermDenied         = errors.New("permission denied")
-	ErrAuthenticationFail = errors.New("authentication fails")
-	ErrAuthorizationFial  = errors.New("authorization fails")
+	ErrAuthenticationFail = errors.New("authentication fails, retry after 5m")
+	ErrAuthorizationFial  = errors.New("authorization fails, retry after 5m")
 )
