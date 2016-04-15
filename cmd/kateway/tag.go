e9bb5aa437fdf6fff5b41f67841a3af078b6d7b0
funkygao <funky.gao@gmail.com>
2016-04-15 20:50:57 +0800

    a basically low mem footprint tag lib

--- cmd/kateway/tag.go
+++ cmd/kateway/tag.go
@@ -1,48 +1,85 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
+	"strings"
 
 	"github.com/funkygao/gafka/mpool"
 )
 
-const MaxTagLen = 100
+const (
+	TagMarkStart = byte(1)
+	TagMarkEnd   = byte(2)
+	TagSeperator = ";" // follow cookie rules a=b;c=d
+)
 
-func isTaggedMessage(msg []byte) bool {
-	return msg[0] == 0
+type MsgTag struct {
+	Name  string
+	Value string
 }
 
-func TagMessage(tags map[string]string, msg []byte) *mpool.Message {
-	b, _ := json.Marshal(tags)
+// TODO perf
+func AddTagToMessage(m *mpool.Message, tags string) {
+	body := make([]byte, len(m.Body))
+	copy(body, m.Body) // FIXME O(N)
 
-	totalLen := len(b) + 1 + len(msg)
-	m := mpool.NewMessage(totalLen)
-	m.Write([]byte{0}) // mark this message is tagged
-	m.Write(b)
-	m.Write(msg)
+	m.Reset()
+	m.Write([]byte{TagMarkStart})
+	m.WriteString(tags)
+	m.Write([]byte{TagMarkEnd})
 
-	return m
+	m.Write(body)
 }
 
-func UntagMessage(msg []byte, tags *map[string]string) ([]byte, error) {
-	if !isTaggedMessage(msg) {
-		return msg, nil
+func ExtractMessageTag(msg []byte) ([]MsgTag, int, error) {
+	tagEnd := bytes.IndexByte(msg, TagMarkEnd)
+	if tagEnd == -1 {
+		// not a tagged message
+		return nil, -1, ErrIllegalTaggedMessage
 	}
 
-	var hi int
-	for idx, c := range msg[1:] {
-		if c == '}' {
-			hi = idx + 1
-			break
+	tag := string(msg[1:tagEnd]) // discard the tag mark start
+	tags := parseMessageTag(tag)
+	return tags, tagEnd + 1, nil
+}
+
+func tagLen(tag string) int {
+	return 2 + len(tag) // TagMarkStart tag TagMarkEnd
+}
+
+func parseMsgTagValue(raw string, allowDoubleQuote bool) (string, bool) {
+	// Strip the quotes, if present.
+	if allowDoubleQuote && len(raw) > 1 && raw[0] == '"' && raw[len(raw)-1] == '"' {
+		raw = raw[1 : len(raw)-1]
+	}
+
+	return raw, true
+}
+
+func parseMessageTag(tag string) []MsgTag {
+	tags := []MsgTag{}
+	kvs := strings.Split(strings.TrimSpace(tag), TagSeperator)
+	if len(kvs) == 1 && kvs[0] == "" {
+		return nil
+	}
+
+	for _, kv := range kvs {
+		kv = strings.TrimSpace(kv)
+		j := strings.Index(kv, "=")
+		if j < 0 {
+			continue
 		}
+		name, value := kv[:j], kv[j+1:]
+		value, ok := parseMsgTagValue(value, true)
+		if !ok {
+			continue
+		}
+
+		tags = append(tags, MsgTag{
+			Name:  name,
+			Value: value,
+		})
 	}
 
-	if hi == 0 {
-		return nil, ErrIllegalTaggedMessage
-	}
-
-	if err := json.Unmarshal(msg[1:hi+1], tags); err != nil {
-		return nil, err
-	}
-	return msg[hi+1:], nil
+	return tags
 }
