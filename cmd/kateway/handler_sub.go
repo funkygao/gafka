db7ec4d86b2870043b4f02d995af4e1e4886ceaf
funkygao <funky.gao@gmail.com>
2016-04-18 09:41:37 +0800

    add a new sub api: bury, which is seperated from sub handler

--- cmd/kateway/handler_sub.go
+++ cmd/kateway/handler_sub.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"strconv"
 
@@ -13,7 +14,165 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// /topics/:appid/:topic/:ver?group=xx&&reset=<newest|oldest>&ack=1&q=<dead|retry>
+// /bury/:appid/:topic/:ver?group=xx&q=yy
+func (this *Gateway) buryHandler(w http.ResponseWriter, r *http.Request,
+	params httprouter.Params) {
+	var (
+		topic      string
+		ver        string
+		myAppid    string
+		hisAppid   string
+		group      string
+		rawTopic   string
+		shadow     string
+		bury       string
+		partition  string
+		partitionN int = -1
+		offset     string
+		offsetN    int64 = -1
+		err        error
+	)
+
+	if options.EnableClientStats {
+		this.clientStates.RegisterSubClient(r)
+	}
+
+	query := r.URL.Query()
+	group = query.Get("group")
+	if !manager.Default.ValidateGroupName(r.Header, group) {
+		this.writeBadRequest(w, "illegal group")
+		return
+	}
+
+	ver = params.ByName(UrlParamVersion)
+	topic = params.ByName(UrlParamTopic)
+	hisAppid = params.ByName(UrlParamAppid)
+	myAppid = r.Header.Get(HttpHeaderAppid)
+
+	bury = r.Header.Get(HttpHeaderMsgBury)
+	if !sla.ValidateShadowName(bury) {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} illegal bury: %s",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err, bury)
+
+		this.writeBadRequest(w, "illegal bury")
+		return
+	}
+
+	// auth
+	if err = manager.Default.AuthSub(myAppid, r.Header.Get(HttpHeaderSubkey),
+		hisAppid, topic, group); err != nil {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} %v",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err)
+
+		this.writeAuthFailure(w, err)
+		return
+	}
+
+	partition = r.Header.Get(HttpHeaderPartition)
+	offset = r.Header.Get(HttpHeaderOffset)
+	if partition == "" || offset == "" {
+		this.writeBadRequest(w, "empty offset or partition")
+		return
+	}
+
+	offsetN, err = strconv.ParseInt(offset, 10, 64)
+	if err != nil || offsetN < 0 {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} offset:%s",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, offset)
+
+		this.writeBadRequest(w, "bad offset")
+		return
+	}
+	partitionN, err = strconv.Atoi(partition)
+	if err != nil || partitionN < 0 {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} partition:%s",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, partition)
+
+		this.writeBadRequest(w, "bad partition")
+		return
+	}
+
+	shadow = query.Get("q")
+
+	log.Debug("bury[%s] %s(%s): {app:%s, bury:%s shadow=%s topic:%s, ver:%s, group:%s partition:%s, offset:%s}",
+		myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, bury, shadow, topic, ver, partition, offset)
+
+	msgLen := int(r.ContentLength)
+	msg := make([]byte, 0, msgLen)
+	if _, err := io.ReadAtLeast(r.Body, msg, msgLen); err != nil {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} %v",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err)
+
+		this.writeBadRequest(w, err.Error())
+		return
+	}
+
+	cluster, found := manager.Default.LookupCluster(hisAppid)
+	if !found {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} cluster not found",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group)
+
+		this.writeBadRequest(w, "invalid appid")
+		return
+	}
+
+	// calculate raw topic according to shadow
+	if shadow != "" {
+		if !sla.ValidateShadowName(shadow) {
+			log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s use:%s} invalid shadow name",
+				myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, shadow)
+
+			this.writeBadRequest(w, "invalid shadow name")
+			return
+		}
+
+		if !manager.Default.IsShadowedTopic(hisAppid, topic, ver, myAppid, group) {
+			log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s use:%s} not a shadowed topic",
+				myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, shadow)
+
+			this.writeBadRequest(w, "register shadow first")
+			return
+		}
+
+		rawTopic = manager.ShadowTopic(shadow, myAppid, hisAppid, topic, ver, group)
+	} else {
+		rawTopic = manager.KafkaTopic(hisAppid, topic, ver)
+	}
+
+	fetcher, err := store.DefaultSubStore.Fetch(cluster, rawTopic,
+		myAppid+"."+group, r.RemoteAddr, "")
+	if err != nil {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} %v",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err)
+
+		this.writeBadRequest(w, err.Error())
+		return
+	}
+
+	shadowTopic := manager.ShadowTopic(bury, myAppid, hisAppid, topic, ver, group)
+	_, _, err = store.DefaultPubStore.SyncPub(cluster, shadowTopic, nil, msg)
+	if err != nil {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} %v",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err)
+
+		this.writeErrorResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// skip this message in the master topic
+	if err = fetcher.CommitUpto(&sarama.ConsumerMessage{
+		Topic:     rawTopic,
+		Partition: int32(partitionN),
+		Offset:    offsetN,
+	}); err != nil {
+		log.Error("bury[%s] %s(%s): {app:%s, topic:%s, ver:%s, group:%s} %v",
+			myAppid, r.RemoteAddr, getHttpRemoteIp(r), hisAppid, topic, ver, group, err)
+
+		this.writeErrorResponse(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// /msgs/:appid/:topic/:ver?group=xx&&reset=<newest|oldest>&ack=1&q=<dead|retry>
 func (this *Gateway) subHandler(w http.ResponseWriter, r *http.Request,
 	params httprouter.Params) {
 	var (
