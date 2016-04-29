ebc0f62faa73b7b010de46c3447a887ff5fee0b7
funkygao <funky.gao@gmail.com>
2016-04-29 18:23:13 +0800

    fix: add v1 to REST call

--- cmd/kateway/play.sh
+++ cmd/kateway/play.sh
@@ -9,41 +9,37 @@ gk clusters -z local -add me -p /kafka_pubsub
 gk clusters -z local -c me -s -public 1 -nickname me
 
 # register a topic
-curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' 'http://localhost:9193/topics/me/app1/foobar/v1?partitions=1&replicas=1'
+curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' 'http://localhost:9193/v1/topics/me/app1/foobar/v1?partitions=1&replicas=1'
 
 # register a shadow topic
-curl -i -XPOST -H'Appid: app14' -H'Subkey: mysubkey' http://localhost:9192/guard/app1/foobar/v1/mygroup1
+curl -i -XPOST -H'Appid: app14' -H'Subkey: mysubkey' http://localhost:9192/v1/guard/app1/foobar/v1/mygroup1
 
 cd cmd/kateway/bench
 make pub
 make sub
 
 # sub a topic
-curl -XGET -H'Appid: app2' -H'Subkey: mysubkey' 'http://localhost:9192/topics/app1/foobar/v1?group=group1&reset=newest&limit=1'
-curl -XGET -H'Appid: app2' -H'Subkey: mysubkey' 'http://localhost:9192/topics/app1/foo/v1?group=group1'
+curl -XGET -H'Appid: app2' -H'Subkey: mysubkey' 'http://localhost:9192/v1/msgs/app1/foobar/v1?group=group1&reset=newest&limit=1'
+curl -XGET -H'Appid: app2' -H'Subkey: mysubkey' 'http://localhost:9192/v1/msgs/app1/foo/v1?group=group1'
 
 # raw sub
 curl -XGET http://localhost:9192/raw/topics/app1/foobar/v1
 
 # pub a topic
-curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d 'hello world!' 'http://localhost:9191/topics/foobar/v1'
-curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d 'hello world!' 'http://localhost:9191/topics/foobar/v1'
-curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d '@/Users/funky/gopkg/src/github.com/funkygao/fae/contrib/resources/dashboard.png' 'http://localhost:9191/topics/foobar/v1'
+curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d 'hello world!' 'http://localhost:9191/v1/msgs/foobar/v1'
+curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d 'hello world!' 'http://localhost:9191/v1/msgs/foobar/v1'
+curl -XPOST -H'Appid: app1' -H'Pubkey: mypubkey' -d '@/Users/funky/gopkg/src/github.com/funkygao/fae/contrib/resources/dashboard.png' 'http://localhost:9191/v1/msgs/foobar/v1'
 
-# raw pub
-curl -XGET -H'Appid: app1' -H'Pubkey: mypubkey' 'http://localhost:9191/raw/topics/foobar/v1'
+# raw sub
+curl -XGET -H'Appid: app1' -H'Subkey: mypubkey' 'http://localhost:9192/v1/raw/topics/foobar/v1'
 
 
 #----------------------
 # play with meta
 #----------------------
-curl http://localhost:9193/help
+curl http://localhost:9193/v1/status
 echo
-curl http://localhost:9193/ver
-echo
-curl http://localhost:9193/stat
-echo
-curl http://localhost:9193/clusters
+curl http://localhost:9193/v1/clusters
 echo
 
 
