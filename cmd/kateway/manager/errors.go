9d630c20747793ac0fc98caf732b6102eef1cce4
funkygao <funky.gao@gmail.com>
2016-05-11 10:37:52 +0800

    the error message can be a helpful guide

--- cmd/kateway/manager/errors.go
+++ cmd/kateway/manager/errors.go
@@ -6,8 +6,7 @@ import (
 
 var (
 	ErrEmptyIdentity      = errors.New("auth with empty identity")
-	ErrPermDenied         = errors.New("permission denied")
 	ErrAuthenticationFail = errors.New("authentication fails, retry after 5m")
 	ErrAuthorizationFail  = errors.New("authorization fails, retry after 5m")
-	ErrInvalidGroup       = errors.New("register group before using it")
+	ErrInvalidGroup       = errors.New("请登录web manager，在'我的App'的该App下注册该group，注册成功后5分钟后生效")
 )
