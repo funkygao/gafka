7f95a830a450aad6de038aad9ec35a15678de9c7
funkygao <funky.gao@gmail.com>
2016-05-12 14:25:38 +0800

    list all the apis defined in amazon swf

--- cmd/swfd/swf/api_v1.go
+++ cmd/swfd/swf/api_v1.go
@@ -5,6 +5,46 @@ func (this *Swf) setupApis() {
 
 	if this.apiServer != nil {
 		this.apiServer.Router().GET("/alive", m(this.apiServer.checkAliveHandler))
+
+		// learned from amazon:
+		// all apis are POST methods because we need json as input params
+		// endpoint e,g. swf.us-east-1.amazonaws.com
+		const opCountClosedWorkflowExecutions = "CountClosedWorkflowExecutions"
+		const opCountOpenWorkflowExecutions = "CountOpenWorkflowExecutions"
+		const opCountPendingActivityTasks = "CountPendingActivityTasks"
+		const opCountPendingDecisionTasks = "CountPendingDecisionTasks"
+		const opDeprecateActivityType = "DeprecateActivityType"
+		const opDeprecateDomain = "DeprecateDomain"
+		const opDeprecateWorkflowType = "DeprecateWorkflowType"
+		const opDescribeActivityType = "DescribeActivityType"
+		const opDescribeDomain = "DescribeDomain"
+		const opDescribeWorkflowExecution = "DescribeWorkflowExecution"
+		const opDescribeWorkflowType = "DescribeWorkflowType"
+		const opGetWorkflowExecutionHistory = "GetWorkflowExecutionHistory"
+		const opListActivityTypes = "ListActivityTypes"
+		const opListClosedWorkflowExecutions = "ListClosedWorkflowExecutions"
+		const opListDomains = "ListDomains"
+		const opListOpenWorkflowExecutions = "ListOpenWorkflowExecutions"
+		const opListWorkflowTypes = "ListWorkflowTypes"
+
+		const opPollForActivityTask = "PollForActivityTask"
+		const opPollForDecisionTask = "PollForDecisionTask"
+
+		const opSignalWorkflowExecution = "SignalWorkflowExecution"
+		const opStartWorkflowExecution = "StartWorkflowExecution"
+		const opTerminateWorkflowExecution = "TerminateWorkflowExecution"
+
+		const opRecordActivityTaskHeartbeat = "RecordActivityTaskHeartbeat"
+
+		const opRegisterActivityType = "RegisterActivityType"
+		const opRegisterDomain = "RegisterDomain"
+		const opRegisterWorkflowType = "RegisterWorkflowType"
+
+		const opRequestCancelWorkflowExecution = "RequestCancelWorkflowExecution"
+		const opRespondActivityTaskCanceled = "RespondActivityTaskCanceled"
+		const opRespondActivityTaskCompleted = "RespondActivityTaskCompleted"
+		const opRespondActivityTaskFailed = "RespondActivityTaskFailed"
+		const opRespondDecisionTaskCompleted = "RespondDecisionTaskCompleted"
 	}
 
 }
