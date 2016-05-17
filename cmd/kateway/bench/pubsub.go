03db414b4384b10df52fd1d3f8c427c88dc5c675
funkygao <funky.gao@gmail.com>
2016-05-17 18:38:02 +0800

    sub benchmark

--- cmd/kateway/bench/pubsub.go
+++ cmd/kateway/bench/pubsub.go
@@ -20,7 +20,9 @@ var (
 	limit    int
 	debug    bool
 	endpoint string
+	group    string
 	msgfile  string
+	subAppid string
 )
 
 func init() {
@@ -32,6 +34,8 @@ func init() {
 	flag.StringVar(&topic, "topic", "", "topic name")
 	flag.StringVar(&endpoint, "ep", "pub.sit.ffan.com:9191", "end point")
 	flag.StringVar(&msgfile, "msgfile", "", "message file to Pub")
+	flag.StringVar(&group, "group", "bench_go", "sub group name")
+	flag.StringVar(&subAppid, "subappid", "", "sub which app's msg")
 	flag.BoolVar(&debug, "debug", false, "debug")
 
 	flag.Parse()
@@ -46,8 +50,8 @@ func main() {
 		stress.RunStress(benchmarkSub)
 
 	case "help":
-		fmt.Println("Pub: go run pubsub.go -mode pub -appid 73 -key xxxx -msgfile msg -ep pub.sit.ffan.com:9191 -topic risk_beacon_test")
-		fmt.Println("Sub: ")
+		fmt.Println("Pub: go run pubsub.go -c1 10 -step 5 -mode pub -appid 73 -key xxxx -msgfile msg -ep pub.sit.ffan.com:9191 -topic risk_beacon_test")
+		fmt.Println("Sub: go run pubsub.go -c1 1 -c2 1 -mode sub -appid 73 -key xxx -ep sub.sit.ffan.com:9192 -topic risk_beacon_test -group bench_go")
 
 	}
 
@@ -61,6 +65,7 @@ func benchmarkPub(seq int) {
 	var opt api.PubOption
 	opt.Topic = topic
 	opt.Ver = ver
+	opt.Async = true
 
 	msg, err := ioutil.ReadFile(msgfile)
 	if err != nil {
@@ -80,5 +85,38 @@ func benchmarkPub(seq int) {
 }
 
 func benchmarkSub(seq int) {
+	cf := api.DefaultConfig(appId, secret)
+	cf.Debug = false
+	cf.Sub.Endpoint = endpoint
+	client := api.NewClient(cf)
 
+	opt := api.SubOption{
+		AppId: subAppid,
+		Topic: topic,
+		Ver:   ver,
+		Group: group,
+	}
+	var i int
+
+	err := client.Sub(opt, func(statusCode int, msg []byte) error {
+		if debug {
+			log.Printf("%s", string(msg))
+		}
+		if statusCode == 200 {
+			stress.IncCounter("ok", 1)
+		} else {
+			stress.IncCounter("fail", 1)
+		}
+
+		i++
+		if i > limit {
+			return api.ErrSubStop
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
