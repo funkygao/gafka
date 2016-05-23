a135f9177fc6931f824de4b7a760fbd0a75898e0
funkygao <funky.gao@gmail.com>
2016-05-23 08:27:19 +0800

    kguard will be able to show version info

--- cmd/kguard/main.go
+++ cmd/kguard/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/funkygao/gafka"
+)
+
 func main() {
+	for _, arg := range os.Args[1:] {
+		if arg == "-v" || arg == "-version" {
+			fmt.Fprintf(os.Stderr, "%s-%s\n", gafka.Version, gafka.BuildId)
+			return
+		}
+	}
+
 	var m Monitor
 	m.Init()
 	m.ServeForever()
