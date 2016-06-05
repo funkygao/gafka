c41ba9c716184b780c26d6e91ebdaba1e1bd1baa
funkygao <funky.gao@gmail.com>
2016-06-05 15:25:17 +0800

    how to use blktrace

--- misc/iowait.sh
+++ misc/iowait.sh
@@ -2,6 +2,10 @@
 # 
 #===========================================
 # Find the processes that cause high IO wait
+#
+# yum -y install blktrace
+# mount -t debugfs none /sys/kernel/debug
+# blktrace -d /dev/sdm1 -o - | blkparse -i -
 #===========================================
 iostat -x 2 5
 for x in `seq 1 1 10`; do ps -eo state,pid,cmd | grep "^D"; echo "----"; sleep 5; done
