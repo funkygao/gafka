8b65f17f564d8bf4c682d19d9c50b443a93ab1ac
funkygao <funky.gao@gmail.com>
2016-06-09 11:07:31 +0800

    script to rate limit conn from a single host with iptables

--- misc/diagnose.sh
+++ misc/diagnose.sh
@@ -6,3 +6,5 @@ badblocks -nsv /dev/<device>
 # disable Transparent Huge Pages (THP)
 if test -f /sys/kernel/mm/redhat_transparent_hugepage/defrag; then echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag fi
 
+# reject connections for port 9999 above 100 from one source IP
+iptables -A INPUT -p tcp --syn --dport 9999 -m connlimit --connlimit-above 100 --connlimit-mask 32 -j REJECT --reject-with tcp-reset 
