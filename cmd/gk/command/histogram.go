9438bfaa1e58ed230f2fa056c0c6d6ec00234a4a
funkygao <funky.gao@gmail.com>
2016-06-19 02:14:06 +0800

    buddy script for 'gk histogram'

--- cmd/gk/command/histogram.go
+++ cmd/gk/command/histogram.go
@@ -16,6 +16,12 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+/*
+#!/bin/sh
+date >> /var/wd/topics_offsets/offsets; /usr/bin/gk topics -z prod -l -plain | grep CUM >> /var/wd/topics_offsets/offsets
+
+date >> /var/wd/topics_offsets/network; consul exec ifconfig bond0 | grep 'RX bytes' | awk '{print $1,$3,$7}' | sort >> /var/wd/topics_offsets/network
+*/
 type Histogram struct {
 	Ui  cli.Ui
 	Cmd string
