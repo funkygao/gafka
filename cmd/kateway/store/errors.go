72d855ff612b789451d38c295ecb13c5568d9b15
funkygao <funky.gao@gmail.com>
2016-06-21 17:04:30 +0800

    for better err msg

--- cmd/kateway/store/errors.go
+++ cmd/kateway/store/errors.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	ErrShuttingDown     = errors.New("server shutting down")
-	ErrBusy             = errors.New("server too busy")
+	ErrBusy             = errors.New("underlying store too busy")
 	ErrTooManyConsumers = errors.New("consumers more than available partitions")
 	ErrRebalancing      = errors.New("rebalancing, please retry after a while")
 	ErrInvalidCluster   = errors.New("invalid cluster")
