3783c235c2b0bbb031f7923c2c294d55da2c460b
funkygao <funky.gao@gmail.com>
2016-06-23 16:00:46 +0800

    benchmark of logging

---
+++ misc/logging/bench.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"time"
+
+	log "github.com/funkygao/log4go"
+)
+
+func init() {
+	log.DeleteFilter("stdout")
+
+	rotateEnabled, discardWhenDiskFull := true, true
+	filer := log.NewFileLogWriter("bench.log", rotateEnabled, discardWhenDiskFull, 0644)
+	filer.SetFormat("[%d %T] [%L] (%S) %M")
+	filer.SetRotateLines(0)
+	filer.SetRotateDaily(true)
+	log.AddFilter("file", log.INFO, filer)
+}
+
+func main() {
+	start := make(chan struct{})
+	var n int64
+	go func() {
+		start <- struct{}{}
+		for {
+			log.Info("hello %s", "world!")
+			n++
+		}
+	}()
+
+	<-start
+	time.Sleep(time.Second * 10)
+	println(n)
+	return
+}
