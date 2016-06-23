4153fcf4960c64bc8ac7a4c972a5c70c45aceab4
funkygao <funky.gao@gmail.com>
2016-06-23 13:21:06 +0800

    http rpc for kateway: coordinate all kateway nodes in a zone

---
+++ cmd/kateway/gateway/rpc.go
@@ -0,0 +1,57 @@
+package gateway
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/funkygao/gafka/zk"
+)
+
+func (this *Gateway) callKateway(kw *zk.KatewayMeta, method string, uri string) (err error) {
+	url := fmt.Sprintf("http://%s/%s", kw.ManAddr, uri)
+
+	var req *http.Request
+	req, err = http.NewRequest(method, url, nil)
+	if err != nil {
+		return
+	}
+
+	var response *http.Response
+	timeout := time.Second * 10
+	client := &http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 1,
+			Proxy:               nil,
+			Dial: (&net.Dialer{
+				Timeout: timeout,
+			}).Dial,
+			DisableKeepAlives:     true,
+			ResponseHeaderTimeout: timeout,
+			TLSHandshakeTimeout:   timeout,
+		},
+	}
+
+	response, err = client.Do(req)
+	if err != nil {
+		return
+	}
+
+	var body []byte
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		err = errors.New(fmt.Sprintf("%s[%s] -> %d %s", method, url, response.StatusCode, string(body)))
+	}
+
+	return
+}
