0d2b22cd93b050db916cfad7f8f12109b0a7d2c1
funkygao <funky.gao@gmail.com>
2016-06-24 15:46:29 +0800

    a handy script to help debug

--- cmd/kateway/demo/pub.go
+++ cmd/kateway/demo/pub.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/funkygao/gafka/cmd/kateway/api/v1"
 )
@@ -11,12 +12,16 @@ var (
 	addr   string
 	msgKey string
 	n      int
+	timeit bool
+	sleep  time.Duration
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", "localhost:9191", "pub kateway addr")
 	flag.IntVar(&n, "n", 50, "run pub how many times")
 	flag.StringVar(&msgKey, "key", "", "pub message key")
+	flag.DurationVar(&sleep, "sleep", time.Millisecond*100, "sleep between each pub")
+	flag.BoolVar(&timeit, "time", false, "time each pub message cost")
 
 	flag.Parse()
 
@@ -30,8 +35,21 @@ func main() {
 		Topic: "foobar",
 		Ver:   "v1",
 	}
+
+	var t time.Time
 	for i := 0; i < n; i++ {
-		err := c.Pub("", []byte("hello world"), opt)
-		fmt.Println(err)
+		if timeit {
+			t = time.Now()
+		}
+		err := c.Pub("", []byte(fmt.Sprintf("hello world[%d]!", i+1)), opt)
+		if timeit {
+			fmt.Println(i, time.Since(t), err)
+		} else if err != nil {
+			fmt.Println(i, err)
+		}
+
+		if sleep > 0 {
+			time.Sleep(sleep)
+		}
 	}
 }
