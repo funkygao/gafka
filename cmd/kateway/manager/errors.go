fb0d6f2f9ca9f0002bf99c39a6ec93a3f7bbb72a
funkygao <funky.gao@gmail.com>
2016-07-11 11:24:58 +0800

    tweak of the err msg

--- cmd/kateway/manager/errors.go
+++ cmd/kateway/manager/errors.go
@@ -9,5 +9,5 @@ var (
 	ErrEmptyIdentity      = errors.New("auth with empty identity")
 	ErrAuthenticationFail = errors.New("authentication fails, retry after 5m")
 	ErrAuthorizationFail  = errors.New("authorization fails, retry after 5m")
-	ErrInvalidGroup       = errors.New("请登录web manager，在'我的App'的该App下注册该group，注册成功后5分钟后生效")
+	ErrInvalidGroup       = errors.New("请登录web manager，在'我的App'的该App下注册该group")
 )
