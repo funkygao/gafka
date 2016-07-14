11994f14681231b420bd648398230fa5dc597cc1
funkygao <funky.gao@gmail.com>
2016-07-14 21:14:51 +0800

    rename

--- ctx/zone.go
+++ ctx/zone.go
@@ -5,17 +5,17 @@ import (
 )
 
 type zone struct {
-	Name       string // prod
-	Zk         string // localhost:2181,localhost:2182
-	InfluxAddr string // localhost:8086
-	SwfAddr    string // swf endpoint, http://192.168.10.134:9195/v1
+	Name        string // prod
+	Zk          string // localhost:2181,localhost:2182
+	InfluxAddr  string // localhost:8086
+	SwfEndpoint string // http://192.168.10.134:9195/v1
 }
 
 func (this *zone) loadConfig(section *ljconf.Conf) {
 	this.Name = section.String("name", "")
 	this.Zk = section.String("zk", "")
 	this.InfluxAddr = section.String("influxdb", "")
-	this.SwfAddr = section.String("swf", "")
+	this.SwfEndpoint = section.String("swf", "")
 	if this.Name == "" {
 		panic("empty zone name not allowed")
 	}
