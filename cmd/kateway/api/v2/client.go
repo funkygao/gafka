9d2847711eb2a576f3a310822c9083a8d24aa0b8
Funky Gao <funky.gao@gmail.com>
2016-08-02 16:12:11 +0800

    api v2 in progress

---
+++ cmd/kateway/api/v2/client.go
@@ -0,0 +1,15 @@
+package api
+
+type Client struct {
+}
+
+func New(options ...func(c *Client) error) (*Client, error) {
+	c := &Client{}
+	for _, option := range options {
+		if err := option(c); err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
+}
