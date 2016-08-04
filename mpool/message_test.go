3f80fd36cbfaeadd56aa045d0ad6454d223b556d
Funky Gao <funky.gao@gmail.com>
2016-08-04 08:26:34 +0800

    add debug log when allocating memory

--- mpool/message_test.go
+++ mpool/message_test.go
@@ -10,11 +10,11 @@ func TestMessageWriteString(t *testing.T) {
 	m := NewMessage(1029)
 	m.WriteString("hello")
 	m.WriteString("world")
-	assert.Equal(t, "helloworld", string(m.Bytes()))
+	assert.Equal(t, "helloworld", string(m.Body))
 
 	m.Reset()
 	err := m.WriteString("yes")
-	assert.Equal(t, "yes", string(m.Bytes()))
+	assert.Equal(t, "yes", string(m.Body))
 	assert.Equal(t, nil, err)
 }
 
@@ -22,7 +22,7 @@ func TestMessageBytes(t *testing.T) {
 	m := NewMessage(1029)
 	m.WriteString("hello")
 	m.WriteString("world")
-	assert.Equal(t, "helloworld", string(m.Bytes()))
+	assert.Equal(t, "helloworld", string(m.Body))
 }
 
 func TestMessageWrite(t *testing.T) {
@@ -30,12 +30,12 @@ func TestMessageWrite(t *testing.T) {
 	m.Write([]byte("hello"))
 	m.Write([]byte(" "))
 	m.Write([]byte("world"))
-	assert.Equal(t, "hello world", string(m.Bytes()))
+	assert.Equal(t, "hello world", string(m.Body))
 }
 
 func TestMessageMixeWriteAndWriteString(t *testing.T) {
 	m := NewMessage(102)
 	m.Write([]byte("hello"))
 	m.WriteString(" world")
-	assert.Equal(t, "hello world", string(m.Bytes()))
+	assert.Equal(t, "hello world", string(m.Body))
 }
