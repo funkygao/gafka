d88da2c4486ceaab92ef8c8ff67212c4ab57a7db
Funky Gao <funky.gao@gmail.com>
2016-09-02 15:37:34 +0800

    play with the smoke test script: add debug pprof

--- cmd/kateway/hh/disk/example/main.go
+++ cmd/kateway/hh/disk/example/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
 	"time"
 
 	"github.com/funkygao/gafka/cmd/kateway/hh/disk"
@@ -9,6 +12,9 @@ import (
 )
 
 func main() {
+	go http.ListenAndServe("localhost:6786", nil)
+	log.Info("pprf ready on http://localhost:6786/debug/pprof")
+
 	cfg := disk.DefaultConfig()
 	cfg.Dir = "hh"
 	s := disk.New(cfg)
@@ -16,20 +22,29 @@ func main() {
 		panic(err)
 	}
 
-	for seq := 0; seq < 5; seq++ {
+	i, j := 5, 5
+	var wg sync.WaitGroup
+
+	for seq := 0; seq < i; seq++ {
+		wg.Add(1)
 		go func(seq int) {
-			for i := 0; i < 5; i++ {
-				if err := s.Append("c1", "t1", []byte("key"),
-					[]byte(fmt.Sprintf("<#%d/%d sent at: %s>", seq, i+1, time.Now()))); err != nil {
+			defer wg.Done()
+
+			for loops := 0; loops < j; loops++ {
+				if err := s.Append("me", "app1.foobar.v1", []byte("key"),
+					[]byte(fmt.Sprintf("<#%d/%d sent at: %s>", seq, loops+1, time.Now()))); err != nil {
 					panic(err)
 				}
 			}
 		}(seq)
 	}
 
-	log.Info("ENTER sleep...")
-	time.Sleep(time.Second)
+	log.Info("%d sent, waiting Append finish...", i*j)
+	wg.Wait()
+	log.Info("all Append done")
+
 	s.Stop()
+	log.Info("Did you see %d messages?", i*j)
 	log.Info("bye!")
 	log.Close()
 }
