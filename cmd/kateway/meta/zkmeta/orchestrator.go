4d30878a528f3e2539f807aa05b9466908471dab
funkygao <funky.gao@gmail.com>
2016-09-13 20:23:31 +0800

    Distribution of underly store clusters to multi-tenants

---
+++ cmd/kateway/meta/zkmeta/orchestrator.go
@@ -0,0 +1,27 @@
+package zkmeta
+
+import (
+	"github.com/funkygao/gafka/zk"
+)
+
+// Distribution of underly store clusters to multi-tenants.
+func (this *zkMetaStore) Clusters() []map[string]string {
+	r := make([]map[string]string, 0)
+
+	this.mu.RLock()
+	defer this.mu.RUnlock()
+
+	this.zkzone.ForSortedClusters(func(zkcluster *zk.ZkCluster) {
+		info := zkcluster.RegisteredInfo()
+		if !info.Public || info.Nickname == "" {
+			// ignored for kateway manager
+			return
+		}
+
+		c := make(map[string]string)
+		c["name"] = info.Name()
+		c["nickname"] = info.Nickname
+		r = append(r, c)
+	})
+	return r
+}
--- cmd/kateway/meta/zkmeta/zk.go
+++ cmd/kateway/meta/zkmeta/zk.go
@@ -207,27 +207,6 @@ func (this *zkMetaStore) ZkChroot(cluster string) string {
 	return ""
 }
 
-func (this *zkMetaStore) Clusters() []map[string]string {
-	r := make([]map[string]string, 0)
-
-	this.mu.RLock()
-	defer this.mu.RUnlock()
-
-	this.zkzone.ForSortedClusters(func(zkcluster *zk.ZkCluster) {
-		info := zkcluster.RegisteredInfo()
-		if !info.Public || info.Nickname == "" {
-			// ignored for kateway manager
-			return
-		}
-
-		c := make(map[string]string)
-		c["name"] = info.Name()
-		c["nickname"] = info.Nickname
-		r = append(r, c)
-	})
-	return r
-}
-
 func (this *zkMetaStore) ClusterNames() []string {
 	this.mu.RLock()
 	r := make([]string, 0, len(this.clusters))
