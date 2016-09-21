27f415def927474ea4b819d067b7464dd6006183
Funky Gao <funky.gao@gmail.com>
2016-09-21 15:48:48 +0800

    at least, it can run now

--- cmd/kateway/demo/java/PubsubDemo/src/main/java/com/foo/App.java
+++ cmd/kateway/demo/java/PubsubDemo/src/main/java/com/foo/App.java
@@ -1,13 +1,22 @@
 package com.foo;
 
+
 /**
- * Hello world!
+ * PubSub demo!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println("Welcome to PubSub!");
+        demoSub();
+    }
+
+    public static void demoSub() {
+
+    }
+
+    public static void demoPub() {
+
     }
 }
