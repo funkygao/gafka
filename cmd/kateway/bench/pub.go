e1ea01b6895dfa6a635a989e802ac18444447ed0
funkygao <funky.gao@gmail.com>
2016-09-22 20:58:08 +0800

    the message content includes topic name for debugging

--- cmd/kateway/bench/pub.go
+++ cmd/kateway/bench/pub.go
@@ -77,8 +77,8 @@ func pubGatewayLoop(seq int) {
 		sz = msgSize + rand.Intn(msgSize)
 		no = atomic.AddInt64(&n, 1)
 
-		msg = fmt.Sprintf("%s w:%s seq:%-2d no:%-10d payload:%s",
-			time.Now(),
+		msg = fmt.Sprintf("topic is %s, %s w:%s seq:%-2d no:%-10d payload:%s",
+			topic, time.Now(),
 			workerId, seq, no, strings.Repeat("X", sz))
 		if tag != "" {
 			opt.Tag = tag
