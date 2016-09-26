cd8ebf29312b2671665ffd4d2ad812b603792bee
Funky Gao <funky.gao@gmail.com>
2016-09-26 16:59:43 +0800

    runnable, but my sub-pkg api declaration is wrong

--- cmd/kateway/swagger.go
+++ cmd/kateway/swagger.go
@@ -1,7 +1,8 @@
 // @APIVersion 0.3
 // @APITitle PubSub
 // @APIDescription A fully-managed real-time secure and reliable RESTful Cloud Pub/Sub streaming message/job service.
+// @BasePath http://127.0.0.1:8080
 package main
 
 // go get -u github.com/yvasiyarov/swagger
-//go:generate swagger -apiPackage "github.com/funkygao/gafka/cmd/kateway/gateway" -mainApiFile main.go
+//go:generate swagger -format go -apiPackage "github.com/funkygao/gafka/cmd/kateway/gateway" -mainApiFile swagger.go
