dcabd052a86fa5ac2ffd2d4b325417bdbbea5fca
Funky Gao <funky.gao@gmail.com>
2016-10-13 13:27:33 +0800

    mkdir a dedicated mirror for 'gk mirror'

---
+++ cmd/gk/command/mirror/pump.go
@@ -0,0 +1,75 @@
+package mirror
+
+import (
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/funkygao/golib/gofmt"
+	"github.com/funkygao/kafka-cg/consumergroup"
+	log "github.com/funkygao/log4go"
+)
+
+func (this *Mirror) pump(sub *consumergroup.ConsumerGroup, pub sarama.AsyncProducer,
+	stop, stopped chan struct{}) {
+	defer func() {
+		log.Info("closing sub...")
+		sub.Close()
+
+		stopped <- struct{}{} // notify others I'm done
+	}()
+
+	active := false
+	backoff := time.Second * 8
+	idle := time.Second * 10
+	for {
+		select {
+		case <-this.quit:
+			return
+
+		case <-stop:
+			// yes sir!
+			return
+
+		case <-time.After(idle):
+			active = false
+			log.Info("idle 10s waiting for new message")
+
+		case msg, ok := <-sub.Messages():
+			if !ok {
+				log.Warn("sub encounters end of message stream")
+				return
+			}
+
+			if !active || this.Debug {
+				log.Info("<-[#%d] T:%s M:%s", this.transferN, msg.Topic, string(msg.Value))
+			}
+			active = true
+
+			pub.Input() <- &sarama.ProducerMessage{
+				Topic: msg.Topic,
+				Key:   sarama.ByteEncoder(msg.Key),
+				Value: sarama.ByteEncoder(msg.Value),
+			}
+			if this.AutoCommit {
+				sub.CommitUpto(msg)
+			}
+
+			// rate limit, never overflood the limited bandwidth between IDCs
+			// FIXME when compressed, the bandwidth calculation is wrong
+			bytesN := len(msg.Topic) + len(msg.Key) + len(msg.Value) + 20 // payload overhead
+			if this.bandwidthRateLimiter != nil && !this.bandwidthRateLimiter.Pour(bytesN) {
+				time.Sleep(backoff)
+				log.Warn("%s -> bandwidth reached, backoff %s", gofmt.ByteSize(this.transferBytes), backoff)
+			}
+
+			this.transferBytes += int64(bytesN)
+			this.transferN++
+			if this.transferN%this.ProgressStep == 0 {
+				log.Info("%s %s %s", gofmt.Comma(this.transferN), gofmt.ByteSize(this.transferBytes), msg.Topic)
+			}
+
+		case err := <-sub.Errors():
+			log.Error("sub %v", err)
+		}
+	}
+}
