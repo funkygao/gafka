24d698d321e9ad1883d035fda4d5c2afb786173f
funkygao <funky.gao@gmail.com>
2016-10-24 23:00:54 +0800

    give appid in err log

--- cmd/kateway/gateway/handler_sub.go
+++ cmd/kateway/gateway/handler_sub.go
@@ -61,7 +61,7 @@ func (this *subServer) subHandler(w http.ResponseWriter, r *http.Request, params
 
 		if !good {
 			// this bad group client is in confinement period
-			log.Error("sub -(%s): group[%s] failure quota exceeded", realIp, group)
+			log.Error("sub -(%s): group[%s] failure quota exceeded", realIp, realGroup)
 			writeQuotaExceeded(w)
 			return
 		}
