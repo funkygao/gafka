af5834e1ab914827d545d2c4859a1f32c66c5ee0
Funky Gao <funky.gao@gmail.com>
2016-11-17 10:52:13 +0800

    hide tag'ed metrics

--- cmd/kguard/monitor/api.go
+++ cmd/kguard/monitor/api.go
@@ -3,6 +3,7 @@ package monitor
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/funkygao/gafka"
 	"github.com/funkygao/go-metrics"
@@ -15,10 +16,32 @@ func (this *Monitor) setupRoutes() {
 	this.router.GET("/metrics", this.metricsHandler)
 }
 
-// GET /metrics
+// GET /metrics?debug=1
 func (this *Monitor) metricsHandler(w http.ResponseWriter, r *http.Request,
 	params httprouter.Params) {
-	b, err := json.Marshal(metrics.DefaultRegistry)
+	debug := r.URL.Query().Get("debug") == "1"
+	if debug {
+		b, err := json.Marshal(metrics.DefaultRegistry)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.Write(b)
+		return
+	}
+
+	out := make(map[string]interface{}, 100)
+	metrics.DefaultRegistry.Each(func(name string, i interface{}) {
+		if strings.HasPrefix(name, "{") {
+			// don't export the tag'ed metrics
+			return
+		}
+
+		out[name] = i
+	})
+	b, err := json.Marshal(out)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
