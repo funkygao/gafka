661e6f2d78c2f97af688b4773bcd7ab29598117c
funkygao <funky.gao@gmail.com>
2016-11-24 20:23:17 +0800

    BUG FIX: haproxy stats number was not matched
    columns must be the same order as haproxy stats output

--- cmd/gk/command/haproxy.go
+++ cmd/gk/command/haproxy.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/funkygao/gafka/ctx"
 	"github.com/funkygao/gocli"
+	"github.com/funkygao/golib/gofmt"
 	"github.com/ryanuber/columnize"
 )
 
@@ -34,12 +36,28 @@ func (this *Haproxy) Run(args []string) (exitCode int) {
 		return 1
 	}
 
-	// initialize the output columns
+	// initialize the output columns, the order MUST match that of haproxy output
 	this.cols = []string{
-		"# pxname", "svname",
-		"bin", "bout",
-		"scur", "req_rate", "rate",
-		"hrsp_1xx", "hrsp_2xx", "hrsp_3xx", "hrsp_4xx", "hrsp_5xx",
+		"# pxname", // proxy name
+		"svname",   // service name
+		//"smax",     // max sessions
+		"stot", // total sessions
+		"bin",  // bytes in
+		"bout", // bytes out
+		//"dreq",  // denied requests
+		//"dresp", // denied response
+		//"ereq",     // request errors
+		//"econ",     // connection errors
+		"wredis", // redispatches (warning)
+		//"rate",     // number of sessions per second over last elapsed second
+		"rate_max", // max number of new sessions per second
+		"hrsp_1xx", // http responses with 1xx code
+		//"hrsp_2xx",
+		//"hrsp_3xx",
+		"hrsp_4xx",
+		"hrsp_5xx",
+		"cli_abrt", // number of data transfers aborted by the client
+		"srv_abrt", // number of data transfers aborted by the server (inc. in eresp)
 	}
 	this.colsMap = make(map[string]struct{})
 	for _, c := range this.cols {
@@ -108,7 +126,11 @@ func (this *Haproxy) fetchStats(statsUri string) {
 				continue
 			}
 
-			vals = append(vals, col)
+			if n, err := strconv.ParseInt(col, 10, 64); err == nil {
+				vals = append(vals, gofmt.Comma(n))
+			} else {
+				vals = append(vals, col)
+			}
 		}
 
 		lines = append(lines, strings.Join(vals, "|"))
