59f9516dac3633e3469618dc4365fc621cc6b083
Funky Gao <funky.gao@gmail.com>
2016-11-24 16:00:48 +0800

    real time track haproxy

--- cmd/gk/command/haproxy.go
+++ cmd/gk/command/haproxy.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/funkygao/gafka/ctx"
 	"github.com/funkygao/gocli"
+	"github.com/ryanuber/columnize"
 )
 
 type Haproxy struct {
@@ -16,6 +18,9 @@ type Haproxy struct {
 	Cmd string
 
 	zone string
+
+	cols    []string
+	colsMap map[string]struct{}
 }
 
 func (this *Haproxy) Run(args []string) (exitCode int) {
@@ -24,6 +29,18 @@ func (this *Haproxy) Run(args []string) (exitCode int) {
 	cmdFlags.StringVar(&this.zone, "z", ctx.DefaultZone(), "")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
+	}
+
+	// initialize the output columns
+	this.cols = []string{
+		"# pxname", "svname",
+		"bin", "bout",
+		"scur", "req_rate", "rate",
+		"hrsp_1xx", "hrsp_2xx", "hrsp_3xx", "hrsp_4xx", "hrsp_5xx",
+	}
+	this.colsMap = make(map[string]struct{})
+	for _, c := range this.cols {
+		this.colsMap[c] = struct{}{}
 	}
 
 	zone := ctx.Zone(this.zone)
@@ -53,9 +70,35 @@ func (this *Haproxy) fetchStats(statsUri string) {
 	swallow(err)
 
 	this.Ui.Info(statsUri)
-	for _, x := range records {
-		this.Ui.Output(fmt.Sprintf("%+v", x))
+	cols := make(map[int]string) // col:name
+
+	lines := []string{strings.Join(this.cols, "|")}
+	for i, row := range records {
+		if i == 0 {
+			// header
+			for j, col := range row {
+				cols[j] = col
+			}
+			continue
+		}
+
+		if (row[0] != "pub" && row[0] != "sub") || row[1] == "BACKEND" || row[1] == "FRONTEND" {
+			continue
+		}
+
+		var vals []string
+		for j, col := range row {
+			if _, present := this.colsMap[cols[j]]; !present {
+				continue
+			}
+
+			vals = append(vals, col)
+		}
+
+		lines = append(lines, strings.Join(vals, "|"))
 	}
+
+	this.Ui.Output(columnize.SimpleFormat(lines))
 }
 
 func (this *Haproxy) Help() string {
