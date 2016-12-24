21d4eb339e8f982f0f9a2cf6618e1ddb33456005
Funky Gao <funky.gao@gmail.com>
2016-12-24 13:29:57 +0800

    limit brokers range

--- cmd/gk/command/balance.go
+++ cmd/gk/command/balance.go
@@ -68,11 +68,12 @@ type Balance struct {
 	interval      time.Duration
 	detailMode    bool
 	host          string
+	atLeastTps    int64
 
 	offsets     map[string]int64 // host => offset sum TODO
 	lastOffsets map[string]int64
 
-	lastHostOffsets map[string]hostOffsetInfo
+	allHostsTps map[string]hostOffsetInfo
 
 	hostOffsetCh chan map[string]hostOffsetInfo // key is host
 	signalsCh    map[string]chan struct{}
@@ -85,6 +86,7 @@ func (this *Balance) Run(args []string) (exitCode int) {
 	cmdFlags.StringVar(&this.cluster, "c", "", "")
 	cmdFlags.DurationVar(&this.interval, "i", time.Second*5, "")
 	cmdFlags.StringVar(&this.host, "host", "", "")
+	cmdFlags.Int64Var(&this.atLeastTps, "over", 0, "")
 	cmdFlags.BoolVar(&this.detailMode, "d", false, "")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
@@ -93,7 +95,7 @@ func (this *Balance) Run(args []string) (exitCode int) {
 	this.signalsCh = make(map[string]chan struct{})
 	this.hostOffsetCh = make(chan map[string]hostOffsetInfo)
 
-	this.lastHostOffsets = make(map[string]hostOffsetInfo)
+	this.allHostsTps = make(map[string]hostOffsetInfo)
 	this.offsets = make(map[string]int64)
 	this.lastOffsets = make(map[string]int64)
 
@@ -123,20 +125,20 @@ func (this *Balance) collectAll(seq int) {
 	for _, _ = range this.signalsCh {
 		offsets := <-this.hostOffsetCh
 		if seq == 0 {
-			// record into lastHostOffsets
+			// record into allHostsTps
 			for host, offsetInfo := range offsets {
-				if _, present := this.lastHostOffsets[host]; !present {
-					this.lastHostOffsets[host] = hostOffsetInfo{host: host, offsetMap: make(map[string]map[structs.TopicPartition]int64)}
+				if _, present := this.allHostsTps[host]; !present {
+					this.allHostsTps[host] = hostOffsetInfo{host: host, offsetMap: make(map[string]map[structs.TopicPartition]int64)}
 				}
 
 				for cluster, tps := range offsetInfo.offsetMap {
-					if _, present := this.lastHostOffsets[host].offsetMap[cluster]; !present {
-						this.lastHostOffsets[host].offsetMap[cluster] = make(map[structs.TopicPartition]int64)
+					if _, present := this.allHostsTps[host].offsetMap[cluster]; !present {
+						this.allHostsTps[host].offsetMap[cluster] = make(map[structs.TopicPartition]int64)
 					}
 
 					for tp, off := range tps {
 						// 1st loop, offset
-						this.lastHostOffsets[host].offsetMap[cluster][tp] = off
+						this.allHostsTps[host].offsetMap[cluster][tp] = off
 					}
 				}
 			}
@@ -146,7 +148,7 @@ func (this *Balance) collectAll(seq int) {
 					for tp, off := range tps {
 						// 2nd loop, qps
 						// FIXME hard coding
-						this.lastHostOffsets[host].offsetMap[cluster][tp] = (off - this.lastHostOffsets[host].offsetMap[cluster][tp]) / int64(this.interval.Seconds())
+						this.allHostsTps[host].offsetMap[cluster][tp] = (off - this.allHostsTps[host].offsetMap[cluster][tp]) / int64(this.interval.Seconds())
 					}
 				}
 			}
@@ -167,13 +169,17 @@ func (this *Balance) drawBalance() {
 		tps  int64
 	}
 	var sortedHosts []hostTps
-	for host, info := range this.lastHostOffsets {
+	for host, info := range this.allHostsTps {
 		sortedHosts = append(sortedHosts, hostTps{host, info.Total()})
 	}
 	sortutil.AscByField(sortedHosts, "tps")
 
 	var hosts []string
 	for _, h := range sortedHosts {
+		if this.atLeastTps > 0 && h.tps < this.atLeastTps {
+			continue
+		}
+
 		hosts = append(hosts, h.host)
 	}
 
@@ -194,7 +200,7 @@ func (this *Balance) drawDetail(sortedHosts []string) {
 
 	for _, host := range sortedHosts {
 		var summary []hostSummary
-		offsetInfo := this.lastHostOffsets[host]
+		offsetInfo := this.allHostsTps[host]
 
 		for cluster, tps := range offsetInfo.offsetMap {
 			for tp, qps := range tps {
@@ -224,11 +230,11 @@ type clusterQps struct {
 func (c clusterQps) String() string {
 	partitions := fmt.Sprintf("%d", c.partitions)
 	if c.qps < 1000 {
-		return fmt.Sprintf("%s/%s/%d", c.cluster, partitions, c.qps)
+		return fmt.Sprintf("%s#%s/%d", c.cluster, partitions, c.qps)
 	} else if c.qps < 5000 {
-		return fmt.Sprintf("%s/%s/%s", c.cluster, partitions, color.Magenta("%d", c.qps))
+		return fmt.Sprintf("%s#%s/%s", c.cluster, partitions, color.Magenta("%d", c.qps))
 	} else if c.qps < 10000 {
-		return fmt.Sprintf("%s/%s/%s", c.cluster, partitions, color.Yellow("%d", c.qps))
+		return fmt.Sprintf("%s#%s/%s", c.cluster, partitions, color.Yellow("%d", c.qps))
 	}
 
 	return fmt.Sprintf("%s/%s/%s", c.cluster, partitions, color.Red("%d", c.qps))
@@ -240,7 +246,7 @@ func (this *Balance) drawSummary(sortedHosts []string) {
 	var totalPartitions int
 	for _, host := range sortedHosts {
 		hostPartitions := 0
-		offsetInfo := this.lastHostOffsets[host]
+		offsetInfo := this.allHostsTps[host]
 		var clusters []clusterQps
 		for cluster, _ := range offsetInfo.offsetMap {
 			clusterTps := offsetInfo.ClusterTotal(cluster)
@@ -333,6 +339,9 @@ Options:
 
     -d
       Display in detailed mode.
+
+    -over number
+      Only display brokers whose TPS over the number.
 
 `, this.Cmd, this.Synopsis(), ctx.ZkDefaultZone())
 	return strings.TrimSpace(help)
