d578659ca3977fd9c94251f9fedbe37fa2a90d56
Funky Gao <funky.gao@gmail.com>
2017-01-13 13:40:57 +0800

    Each production server should enable diagnostics.

---
+++ diagnostics/diag.go
@@ -0,0 +1,15 @@
+package diagnostics
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	log "github.com/funkygao/log4go"
+)
+
+var HttpAddr = "localhost:10002"
+
+func Start() {
+	go http.ListenAndServe(HttpAddr, nil)
+	log.Info("pprof ready on %s", HttpAddr)
+}
---
+++ diagnostics/doc.go
@@ -0,0 +1,5 @@
+// Package diagnostics provides an HTTP endpoint for a server providing
+// diagnostics and statistics for a given task.
+//
+// Each production server should enable diagnostics.
+package diagnostics
