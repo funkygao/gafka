dc3f39ce0c30aca87a900b5a94dfa3ef512aae0a
Funky Gao <funky.gao@gmail.com>
2017-01-19 14:22:01 +0800

    user can specify interval

--- cmd/gk/command/systool.go
+++ cmd/gk/command/systool.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/funkygao/columnize"
 	"github.com/funkygao/gocli"
+	"github.com/funkygao/golib/color"
 	"github.com/funkygao/golib/gofmt"
 	"github.com/shirou/gopsutil/disk"
 )
@@ -21,23 +22,25 @@ type Systool struct {
 func (this *Systool) Run(args []string) (exitCode int) {
 	var (
 		diskTool bool
+		interval time.Duration
 	)
 	cmdFlags := flag.NewFlagSet("systool", flag.ContinueOnError)
 	cmdFlags.Usage = func() { this.Ui.Output(this.Help()) }
 	cmdFlags.BoolVar(&diskTool, "d", false, "")
+	cmdFlags.DurationVar(&interval, "i", time.Second*3, "")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
 	}
 
 	if diskTool {
-		this.runDiskTool()
+		this.runDiskTool(interval)
 		return
 	}
 
 	return
 }
 
-func (*Systool) runDiskTool() {
+func (*Systool) runDiskTool(interval time.Duration) {
 	var lastStats = make(map[string]disk.IOCountersStat)
 	for {
 		stats, err := disk.IOCounters()
@@ -71,10 +74,21 @@ func (*Systool) runDiskTool() {
 				stat.WeightedIO -= last.WeightedIO
 			}
 
+			var (
+				rbytes = gofmt.ByteSize(stat.ReadBytes).String()
+				wbytes = gofmt.ByteSize(stat.WriteBytes).String()
+			)
+			if strings.HasSuffix(rbytes, "MB") || strings.HasSuffix(rbytes, "GB") || strings.HasSuffix(rbytes, "TB") {
+				rbytes = color.Red("%-10s", rbytes)
+			}
+			if strings.HasSuffix(wbytes, "MB") || strings.HasSuffix(wbytes, "GB") || strings.HasSuffix(wbytes, "TB") {
+				wbytes = color.Red("%-11s", wbytes)
+			}
+
 			lines = append(lines, fmt.Sprintf("%s|%d|%s|%s|%d|%d|%d|%d|%d|%d|%d|%d",
 				stat.Name,
 				stat.IopsInProgress,
-				gofmt.ByteSize(stat.ReadBytes), gofmt.ByteSize(stat.WriteBytes),
+				rbytes, wbytes,
 				stat.ReadCount, stat.WriteCount,
 				stat.MergedReadCount, stat.MergedWriteCount,
 				stat.ReadTime, stat.WriteTime,
@@ -86,9 +100,9 @@ func (*Systool) runDiskTool() {
 		fmt.Println(columnize.SimpleFormat(lines))
 
 		if len(lastStats) == 0 {
-			time.Sleep(time.Second * 5)
+			time.Sleep(interval + time.Second*3)
 		} else {
-			time.Sleep(time.Second * 3)
+			time.Sleep(interval)
 		}
 
 		lastStats = stats
@@ -110,6 +124,9 @@ Options:
 
     -d
       Disk diagnostics
+
+    -i interval
+      e,g. -i 3s
 
 `, this.Cmd, this.Synopsis())
 	return strings.TrimSpace(help)
