f2aa85cdba8e1289cba12fc07d8fe631039f486c
Funky Gao <funky.gao@gmail.com>
2017-01-20 09:49:22 +0800

    add doc

--- ver.go
+++ ver.go
@@ -1,8 +1,14 @@
-// package gafka is a full ecosystem built for real-time cloud PubSub system.
+// Package gafka is a full ecosystem built for real-time cloud PubSub system.
 package gafka
 
 var (
-	Version string = "unknown"
-	BuildId string = "?"
-	BuiltAt string = "1970"
+	// Version is the unified version of the whole gafka project.
+	// Each component shares the same version info.
+	Version = "unknown"
+
+	// BuildId is the SCM commit id.
+	BuildId = "?"
+
+	// BuiltAt is the time when build.sh was run.
+	BuiltAt = "1970"
 )
