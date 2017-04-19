7313522bcf6325f95fdbfcb9e68232ad038c35a8
Funky Gao <funky.gao@gmail.com>
2017-04-19 11:10:08 +0800

    'gk trace' ok

--- cmd/gk/command/trace.go
+++ cmd/gk/command/trace.go
@@ -2,36 +2,51 @@ package command
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/funkygao/gafka/ctx"
 	"github.com/funkygao/gafka/zk"
 	"github.com/funkygao/gocli"
+	"github.com/funkygao/golib/bjtime"
+	"github.com/funkygao/golib/color"
+	"github.com/funkygao/golib/gofmt"
 )
+
+type clusterMessage struct {
+	*sarama.ConsumerMessage
+	cluster string
+}
 
 type Trace struct {
 	Ui  cli.Ui
 	Cmd string
 
-	lastDays int64 // peek the most recent N messages
-	column   string
-	grep     string
+	lastDuration time.Duration
+	grep         string
 }
+
+var defaultTopicRetention = time.Hour * 24 * 7
 
 func (this *Trace) Run(args []string) (exitCode int) {
 	var (
-		zone string
-		from string
+		zone      string
+		from      string
+		highlight bool
+		pretty    bool
 	)
 	cmdFlags := flag.NewFlagSet("trace", flag.ContinueOnError)
 	cmdFlags.Usage = func() { this.Ui.Output(this.Help()) }
 	cmdFlags.StringVar(&zone, "z", ctx.ZkDefaultZone(), "")
 	cmdFlags.StringVar(&from, "from", "", "")
-	cmdFlags.Int64Var(&this.lastDays, "last", 3, "")
+	cmdFlags.DurationVar(&this.lastDuration, "last", time.Hour, "")
+	cmdFlags.BoolVar(&highlight, "highlight", false, "")
 	cmdFlags.StringVar(&this.grep, "grep", "", "")
+	cmdFlags.BoolVar(&pretty, "pretty", false, "")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
 	}
@@ -42,7 +57,9 @@ func (this *Trace) Run(args []string) (exitCode int) {
 		return 2
 	}
 
-	msgChan := make(chan *sarama.ConsumerMessage, 20000)
+	this.Ui.Infof("seeking %s", this.grep)
+
+	msgChan := make(chan clusterMessage, 2000)
 	zkzone := zk.NewZkZone(zk.DefaultConfig(zone, ctx.ZoneZkAddrs(zone)))
 	for _, clusterTopic := range strings.Split(from, ",") {
 		tuples := strings.SplitN(clusterTopic, "@", 2)
@@ -50,27 +67,59 @@ func (this *Trace) Run(args []string) (exitCode int) {
 			panic(clusterTopic)
 		}
 
-		cluster, topic := tuples[0], tuples[1]
+		cluster, topic := tuples[1], tuples[0]
 		zkcluster := zkzone.NewCluster(cluster)
 		this.consumeCluster(zkcluster, topic, msgChan)
 	}
 
 	grepB := []byte(this.grep)
-	for msg := range msgChan {
-		if bytes.Contains(msg.Value, grepB) {
-			this.Ui.Outputf("%20s %s", msg.Topic, string(msg.Value))
+	var n int64
+	progressInterval := time.Second * 30
+	tick := time.NewTicker(progressInterval)
+	var prettyJSON bytes.Buffer
+	var content string
+	for {
+		select {
+		case msg := <-msgChan:
+			n++
+			if bytes.Contains(msg.Value, grepB) {
+				progressInterval = time.Minute
+				tick = time.NewTicker(progressInterval)
+
+				if highlight {
+					msg.Value = bytes.Replace(msg.Value, grepB, []byte(color.Red(this.grep)), -1)
+				}
+
+				if pretty {
+					if err := json.Indent(&prettyJSON, msg.Value, "", "    "); err != nil {
+						this.Ui.Error(err.Error())
+						content = string(msg.Value)
+					} else {
+						content = string(prettyJSON.Bytes())
+					}
+				}
+
+				prettyJSON.Reset()
+
+				this.Ui.Infof("%s %d/%d", color.Green("%40s", fmt.Sprintf("%s@%s", msg.Topic, msg.cluster)), msg.Partition, msg.Offset)
+				this.Ui.Output(content)
+			}
+
+		case <-tick.C:
+			this.Ui.Outputf("%16s msgs received %s", gofmt.Comma(n), bjtime.TimeToString(time.Now()))
 		}
 	}
 
 	return
 }
 
-func (this *Trace) consumeCluster(zkcluster *zk.ZkCluster, topicPattern string,
-	msgChan chan *sarama.ConsumerMessage) {
+func (this *Trace) consumeCluster(zkcluster *zk.ZkCluster, topic string, msgChan chan<- clusterMessage) {
 	brokerList := zkcluster.BrokerList()
 	if len(brokerList) == 0 {
+		this.Ui.Warnf("cluster[%s] has no live brokers", zkcluster.Name())
 		return
 	}
+
 	kfk, err := sarama.NewClient(brokerList, sarama.NewConfig())
 	if err != nil {
 		this.Ui.Output(err.Error())
@@ -85,25 +134,19 @@ func (this *Trace) consumeCluster(zkcluster *zk.ZkCluster, topicPattern string,
 	}
 
 	for _, t := range topics {
-		if patternMatched(t, topicPattern) {
+		if t == topic {
 			go this.consumeTopic(zkcluster, kfk, t, msgChan)
 		}
 	}
 }
 
-func (this *Trace) consumeTopic(zkcluster *zk.ZkCluster, kfk sarama.Client, topic string,
-	msgCh chan *sarama.ConsumerMessage) {
+func (this *Trace) consumeTopic(zkcluster *zk.ZkCluster, kfk sarama.Client, topic string, msgCh chan<- clusterMessage) {
 	consumer, err := sarama.NewConsumerFromClient(kfk)
-	if err != nil {
-		panic(err)
-	}
+	swallow(err)
 	defer consumer.Close()
 
-	// all partitions
 	partitions, err := kfk.Partitions(topic)
-	if err != nil {
-		panic(err)
-	}
+	swallow(err)
 
 	for _, p := range partitions {
 		var offset int64
@@ -113,8 +156,7 @@ func (this *Trace) consumeTopic(zkcluster *zk.ZkCluster, kfk sarama.Client, topi
 		oldestOffset, err := kfk.GetOffset(topic, p, sarama.OffsetOldest)
 		swallow(err)
 
-		// most topics have retention of 7 days
-		offset = oldestOffset + (latestOffset-oldestOffset)*this.lastDays/7
+		offset = latestOffset - (latestOffset-oldestOffset)*int64(this.lastDuration.Seconds())/int64(defaultTopicRetention.Seconds())
 		if offset <= 0 {
 			this.Ui.Warnf("%s/%d empty", topic, p)
 			continue
@@ -125,7 +167,7 @@ func (this *Trace) consumeTopic(zkcluster *zk.ZkCluster, kfk sarama.Client, topi
 }
 
 func (this *Trace) consumePartition(zkcluster *zk.ZkCluster, kfk sarama.Client, consumer sarama.Consumer,
-	topic string, partitionId int32, msgCh chan *sarama.ConsumerMessage, offset int64) {
+	topic string, partitionId int32, msgCh chan<- clusterMessage, offset int64) {
 	p, err := consumer.ConsumePartition(topic, partitionId, offset)
 	if err != nil {
 		this.Ui.Error(fmt.Sprintf("%s %s/%d: offset=%d %v", zkcluster.Name(), topic, partitionId, offset, err))
@@ -134,7 +176,7 @@ func (this *Trace) consumePartition(zkcluster *zk.ZkCluster, kfk sarama.Client, 
 	defer p.Close()
 
 	for msg := range p.Messages() {
-		msgCh <- msg
+		msgCh <- clusterMessage{ConsumerMessage: msg, cluster: zkcluster.Name()}
 	}
 }
 
@@ -159,9 +201,16 @@ Options:
 
     -grep pattern
 
-    -last days
-      Trace messages from last N days.
-      Default 3
+    -last duration
+      Trace messages since last duration ago.
+      Default 1h
+      e,g.
+      -last 5m
+      -last 5h
+
+    -highlight true|false
+
+    -pretty
 
 `, this.Cmd, this.Synopsis(), ctx.ZkDefaultZone())
 	return strings.TrimSpace(help)
