cc3abea3736ad22cfa8c16ebd10b468f3ef742d1
Funky Gao <funky.gao@gmail.com>
2017-06-14 11:20:30 +0800

    nil

--- cmd/gk/command/cpu.go
+++ cmd/gk/command/cpu.go
@@ -15,17 +15,13 @@ type Cpu struct {
 }
 
 func (this *Cpu) Run(args []string) (exitCode int) {
+	// might not work under VM
 	fmt.Println(cpuid.CPU.BrandName, "Family", cpuid.CPU.Family, "Model:", cpuid.CPU.Model)
 	fmt.Println("PhysicalCores:", cpuid.CPU.PhysicalCores, "ThreadsPerCore:", cpuid.CPU.ThreadsPerCore, "LogicalCores:", cpuid.CPU.LogicalCores)
 	fmt.Println("Cacheline bytes:", cpuid.CPU.CacheLine)
 	fmt.Println("L1 Data Cache:", gofmt.ByteSize(cpuid.CPU.Cache.L1D), "Instruction Cache:", gofmt.ByteSize(cpuid.CPU.Cache.L1D))
 	fmt.Println("L2 Cache:", gofmt.ByteSize(cpuid.CPU.Cache.L2))
 	fmt.Println("L3 Cache:", gofmt.ByteSize(cpuid.CPU.Cache.L3))
-
-	// Test if we have a specific feature:
-	if cpuid.CPU.SSE() {
-		fmt.Println("We have Streaming SIMD Extensions")
-	}
 
 	return
 }
