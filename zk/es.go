445a8726a3c6f4252afc68c7be1653270e177504
Funky Gao <funky.gao@gmail.com>
2017-06-16 07:47:02 +0800

    zk es

--- zk/es.go
+++ zk/es.go
@@ -1,14 +1,24 @@
 package zk
 
+import (
+	"fmt"
+)
+
 type EsCluster struct {
 	Name   string
 	zkzone *ZkZone
 }
 
 func (ec *EsCluster) AddNode(hostPort string) error {
-	return nil
+	path := fmt.Sprintf("%s/%s/node/%s", esRoot, ec.Name, hostPort)
+	if err := ec.zkzone.ensureParentDirExists(path); err != nil {
+		return err
+	}
+
+	return ec.zkzone.CreatePermenantZnode(path, nil)
 }
 
 func (ec *EsCluster) Nodes() []string {
-	return nil
+	path := fmt.Sprintf("%s/%s/node", esRoot, ec.Name)
+	return ec.zkzone.children(path)
 }
