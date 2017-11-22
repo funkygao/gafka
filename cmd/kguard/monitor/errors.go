d038fc21429991538b8a7b8f9d1d75a95ed88a6f
chenlei106 <chenlei106@wanda.cn>
2017-11-22 19:07:52 +0800

    add kfk monitor interface 5: get kfk host's info (disk space/MB, cpu cores, memory/MB)
    // @rest POST /kfk/hosts

--- cmd/kguard/monitor/errors.go
+++ cmd/kguard/monitor/errors.go
@@ -5,9 +5,12 @@ import (
 )
 
 var (
-	ErrClusterNotExist  = errors.New("cluster not exist")
-	ErrBrokerIdNotExist = errors.New("broker id not exist")
-	ErrTopicNotExist    = errors.New("topic not exist")
-	ErrGroupNotExist    = errors.New("group not exist")
-	ErrGroupNotOnTopic  = errors.New("group not on this topic")
+	ErrClusterNotExist   = errors.New("cluster not exist")
+	ErrBrokerIdNotExist  = errors.New("broker id not exist")
+	ErrTopicNotExist     = errors.New("topic not exist")
+	ErrGroupNotExist     = errors.New("group not exist")
+	ErrGroupNotOnTopic   = errors.New("group not on this topic")
+	ErrHostConsulDead    = errors.New("host consul dead")
+	ErrHostNotExist      = errors.New("host not exist")
+	ErrInvalidConsulLine = errors.New("invliad consul line")
 )
