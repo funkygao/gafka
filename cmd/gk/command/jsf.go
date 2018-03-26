8be91fb2fe2ad52248015f306ed7687a317dcd49
Funky Gao <funky.gao@gmail.com>
2018-03-26 17:05:04 +0800

    still struggle with java parser

--- cmd/gk/command/jsf.go
+++ cmd/gk/command/jsf.go
@@ -3,17 +3,23 @@ package command
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"bramp.net/antlr4/java" // Precompiled Go versions of Java grammar
+	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/funkygao/gocli"
+	//"github.com/funkygao/pretty"
 )
+
+//   go:generate java -jar /Users/funky/bin/antlr-4.7-complete.jar -Dlanguage=Go -o parser template/Java.g4
 
 type Jsf struct {
 	Ui  cli.Ui
 	Cmd string
+
+	*java.BaseJavaParserListener // https://godoc.org/bramp.net/antlr4/java#BaseJavaParserListener
 }
 
 func (this *Jsf) Run(args []string) (exitCode int) {
@@ -28,12 +34,12 @@ func (this *Jsf) Run(args []string) (exitCode int) {
 		return 2
 	}
 
-	this.parseRootDir(args[len(args)-1])
+	this.scanJsfServices(args[len(args)-1])
 
 	return
 }
 
-func (this *Jsf) parseRootDir(root string) {
+func (this *Jsf) scanJsfServices(root string) {
 	swallow(filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
 			return err
@@ -41,20 +47,34 @@ func (this *Jsf) parseRootDir(root string) {
 		if f.IsDir() {
 			return nil
 		}
-		if !strings.HasSuffix(f.Name(), ".java") {
+		if !strings.HasSuffix(strings.ToLower(f.Name()), "service.java") {
 			return nil
 		}
 
-		fmt.Println(f.Name())
-
-		_, err = ioutil.ReadFile(path)
-		if err != nil {
-			return err
-		}
+		// https://blog.gopheracademy.com/advent-2017/parsing-with-antlr4-and-go/
+		// https://github.com/bramp/antlr4-grammars
+		is, e := antlr.NewFileStream(path)
+		swallow(e)
+		// create the lexer
+		lexer := java.NewJavaLexer(is)
+		stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+		// create the parser
+		parser := java.NewJavaParser(stream)
+		parser.BuildParseTrees = true
+		//parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+		// walk the tree
+		antlr.ParseTreeWalkerDefault.Walk(this, parser.CompilationUnit())
 
 		return nil
-
 	}))
+}
+
+func (this *Jsf) EnterAnnotation(ctx *java.AnnotationContext) {
+	//this.Ui.Output(fmt.Sprintf("%# v", pretty.Formatter(ctx)))
+}
+
+func (this *Jsf) EnterInterfaceMethodDeclaration(c *java.InterfaceMethodDeclarationContext) {
+	fmt.Println(c)
 }
 
 func (*Jsf) Synopsis() string {
