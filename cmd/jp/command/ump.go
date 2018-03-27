de94121ccd829d974def2e22e8c466a0dae9b75b
Funky Gao <funky.gao@gmail.com>
2018-03-27 14:57:05 +0800

    直接打开某个ump key的页面

--- cmd/jp/command/ump.go
+++ cmd/jp/command/ump.go
@@ -6,16 +6,20 @@ import (
 	"strings"
 
 	"github.com/funkygao/gocli"
-	//"github.com/pkg/browser"
+	"github.com/pkg/browser"
 )
 
 type Ump struct {
 	Ui  cli.Ui
 	Cmd string
+
+	appName     string
+	umpRegistry map[string]string // appName:appId
 }
 
 func (this *Ump) Run(args []string) (exitCode int) {
 	cmdFlags := flag.NewFlagSet("ump", flag.ContinueOnError)
+	cmdFlags.StringVar(&this.appName, "app", "", "")
 	cmdFlags.Usage = func() { this.Ui.Output(this.Help()) }
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
@@ -26,6 +30,11 @@ func (this *Ump) Run(args []string) (exitCode int) {
 		return 2
 	}
 
+	this.umpRegistry = map[string]string{
+		"eclp_goods":  "5397",
+		"eclp_master": "5390",
+	}
+
 	keys := strings.Split(args[len(args)-1], ",")
 	this.showReport(keys)
 
@@ -33,6 +42,11 @@ func (this *Ump) Run(args []string) (exitCode int) {
 }
 
 func (this *Ump) showReport(keys []string) {
+	for _, k := range keys {
+		url := fmt.Sprintf("http://ump.jd.com/performanceReport/initPage.action?queryMap.appName=%s&queryMap.appId=%s&queryMap.accessKey=%s&queryMap.analysisFrequency=1&queryMap.departCode=33138&queryMap.groupId=",
+			this.appName, this.umpRegistry[this.appName], k)
+		browser.OpenURL(url)
+	}
 }
 
 func (*Ump) Synopsis() string {
@@ -44,6 +58,14 @@ func (this *Ump) Help() string {
 Usage: %s ump key
 
     %s
+
+e,g. jp ump -app eclp-goods com.jd.eclp.master.goods.service.impl.GoodsServiceImpl.getGoods
+
+Options:
+
+    -app appName
+      e,g. eclp-goods
+      Valid apps: eclp-goods, eclp-master
 
 `, this.Cmd, this.Synopsis())
 	return strings.TrimSpace(help)
